c55554a70e447ac38783a397452178ac3fc32dbb
Agricog <mick@mickstevenson.com>
2026-01-12 23:05:44 +0000

    Create index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Pages Index
+ * Barrel export for all page components
+ */
+
+// Portal pages
+export { PortalHome } from './portal/PortalHome'
+export { ProjectDetail } from './portal/ProjectDetail'
+
+// Dashboard pages
+export { DashboardHome } from './dashboard/DashboardHome'
+export { ProjectList } from './dashboard/ProjectList'
+export { AnalyticsDashboard } from './dashboard/AnalyticsDashboard'
