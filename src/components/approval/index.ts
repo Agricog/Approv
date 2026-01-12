7b55f31801fa63a50d18b8db7eb56d9967318baf
Agricog <mick@mickstevenson.com>
2026-01-12 21:38:26 +0000

    Create index.ts

---
+++ src/components/approval/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Approval Components Index
+ * Barrel export for approval-related components
+ */
+
+export { ApprovalPage } from './ApprovalPage'
+export { ApprovalHeader, ApprovalHeaderCompact } from './ApprovalHeader'
+export type { ApprovalHeaderProps, ApprovalHeaderCompactProps } from './ApprovalHeader'
+
+export { DeliverablePreview } from './DeliverablePreview'
+export type { DeliverablePreviewProps } from './DeliverablePreview'
+
+export { ApprovalActions, ApprovalActionsCompact } from './ApprovalActions'
+export type { ApprovalActionsProps, ApprovalActionsCompactProps } from './ApprovalActions'
+
+export { ChangesModal } from './ChangesModal'
+export type { ChangesModalProps } from './ChangesModal'
+
+export { ApprovalConfirmed } from './ApprovalConfirmed'
+export type { ApprovalConfirmedProps } from './ApprovalConfirmed'
+
+export { ApprovalExpired } from './ApprovalExpired'
+export type { ApprovalExpiredProps } from './ApprovalExpired'
