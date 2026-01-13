7ef527be3ee648d199df57aa8b8e783e557f6807
Agricog <mick@mickstevenson.com>
2026-01-13 09:39:03 +0000

    Create index.ts

---
+++ api/src/routes/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Routes Index
+ * Barrel exports for all route modules
+ */
+
+export { healthRoutes } from './health.js'
+export { csrfRoutes } from './csrf.js'
+export { approvalRoutes } from './approvals.js'
+export { projectRoutes } from './projects.js'
+export { dashboardRoutes } from './dashboard.js'
+export { portalRoutes } from './portal.js'
+export { webhookRoutes } from './webhooks.js'
+export { notificationRoutes } from './notifications.js'
