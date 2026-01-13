09f634ab6a6105901f2050a7a2ff18b781272b28
Agricog <mick@mickstevenson.com>
2026-01-13 10:59:26 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App'
 import './index.css'
 
 // Clerk publishable key
-const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || 'pk_test_ZmFzdC1kYW5lLTU2LmNsZXJrLmFjY291bnRzLmRldiQ'
 
 if (!CLERK_PUBLISHABLE_KEY) {
   console.warn('Missing VITE_CLERK_PUBLISHABLE_KEY - authentication will not work')
